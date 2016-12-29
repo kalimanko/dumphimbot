f250b73900bd9212ed709108edd111daf36c337d
bbullek <bbullek@live.com>
2016-12-29 12:51:26 -0500

    Added p5 directory with API setup

---
+++ giphy_api/sketch.js
@@ -0,0 +1,20 @@
+/*
+ * This script uses the Giphy API with the p5 createImg() function to display GIFs
+ * on a webpage.
+ *
+ * Host: api.giphy.com
+ * Public beta key: dc6zaTOxFJmzC
+ * Path: /v1/gifs/search
+ */
+ 
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var query = "funny+cat"; // Use the + character for phrases consisting of 2+ words
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
