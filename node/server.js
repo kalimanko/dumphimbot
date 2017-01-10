a43447b85b2ce850a84b0100dcf366db84bc27a7
bbullek <bbullek@live.com>
2017-01-10 16:28:30 -0500

    Running server.js from command line now uploads to Twitter

--- node/server.js
+++ node/server.js
@@ -6,3 +6,19 @@ var Twit = require('twit');
 // Authenticate with OAuth
 var config = require('./config'); // File is hidden for security reasons
 var T = new Twit(config);
+
+// Support for posting to Twitter
+var tweet = { 
+	status: 'Hello world!' 
+};
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted (err, data, response) {
+	if (err) {
+		console.log("Error");
+	} else {
+		// Print status & info of the newly posted tweet
+		console.log(data);
+	}
+};
