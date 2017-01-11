ef0334f84bff916e83f7cda2aed75b81b726e363
bbullek <bbullek@live.com>
2017-01-11 16:54:26 -0500

    Edited script to schedule tweets within a configurable interval

--- node/server.js
+++ node/server.js
@@ -7,18 +7,31 @@ var Twit = require('twit');
 var config = require('./config'); // File is hidden for security reasons
 var T = new Twit(config);
 
-// Support for posting to Twitter
-var tweet = { 
-	status: 'Hello world!' 
-};
+postTweet();
 
-T.post('statuses/update', tweet, tweeted);
+// Schedule tweets to post once per day
+var numMillisecondsPerDay = 24 * 60 * 60 * 1000;
+var twenty = 1000 * 20;
+setInterval(postTweet, twenty);
+postTweet();
 
-function tweeted (err, data, response) {
-	if (err) {
-		console.log("Error");
-	} else {
-		// Print status & info of the newly posted tweet
-		console.log(data);
+// Post to Twitter
+function postTweet() {
+	var r = Math.floor(Math.random() * 100);
+	
+	var tweet = {
+		status: 'Hello world! ' + r
+	};
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted (err, data, response) {
+		if (err) {
+			console.log("Error");
+		} else {
+			// Print status & info of the new tweet
+			console.log(data);
+		}
 	}
 };
+
